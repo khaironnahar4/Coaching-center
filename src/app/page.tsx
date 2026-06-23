fe46b12dac26bdf6d7b03d57331bf1cda12b7524
khaironnahar4 <khaironnahar247@gmail.com>
2026-06-23 09:46:42 +0600

    Add Prisma integration with schema and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import Link from "next/link"
+
 const Homepage = () => {
   return (
-    <div className=''></div>
+    <div className='flex items-center gap-4'>
+      <Link className="bg-blue-500 text-white px-4 py-2 rounded" href="/admin">Admin Panel</Link>
+      <Link className="bg-green-500 text-white px-4 py-2 rounded" href="/parent">Parent Page</Link>
+      <Link className="bg-purple-500 text-white px-4 py-2 rounded" href="/teacher">Teacher Page</Link>
+    </div>
   )
 }
 
